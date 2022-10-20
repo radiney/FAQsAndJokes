d191f44df2c636045a5e0e81b275d6e739ca158d
radiney <99511595+radiney@users.noreply.github.com>
2022-10-19 20:09:12 -0700

    Initial Commit

---
+++ FAQsAndJokes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FAQsAndJokes
+//
+//  Created by user217360 on 10/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
